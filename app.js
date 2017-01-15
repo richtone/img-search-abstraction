c0ab0eb928e10b35155ec2e0c1a4dfe63d98adf0
Richard <richard.turza@gmail.com>
2017-01-15 00:27:02 +0000

    finished product

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 "use strict";
+
 var express = require("express");
 var gis = require('node-google-image-search');
-var dotenv = require("dotenv");
-/*
-var result = {  "url" :       undefined,
-                "snippet" :   42,
-                "thumbnail" : null
-             };
-*/
+var mongodb = require("mongodb").MongoClient;
+
 var port = process.env.PORT || 8080;
-var imgur = "https://api.imgur.com/3"
-var authHeader = "Authorization: Client-ID 520786dbc77a534";
+var appDB = undefined;
+var searchTerms = undefined;
+var dbURL = process.env.MONGOLAB_URI;
+
+mongodb.connect(dbURL, (err, db) => {
+    
+    if (err) {
+    console.log("Unable to connect to the mongoDB. Error:", err);
+    } else {
+    console.log("Connection established to mongoDB");
+    }
+    appDB = db;
+    searchTerms = db.collection("searchTerms");
+});
+
 
 express()
+.get("/", (req,res) => {
+    res.end();
+})
 .get("/api/imagesearch/:search_string", (req, res) => {
     let ss = req.params.search_string;
+    let offset = 10*req.query.offset || 10; // zistíme parameter "offset" z req.query objektu (zoznam parametrov)
+    let response = { "term" : ss, "when" : (new Date).toISOString() };
+    
+    searchTerms
+    .insert(response,
+    (err, data) => {
+        if (err) throw err;
+        console.log(response," zapísané do DB");
+    });
     
     gis(ss, data => {
-        
-        let result = data.map((item,index) => {
+
+        let results = data.map((item,index) => {
             return {
                         "url" : item.link,
                         "snippet" : item.snippet,
@@ -26,11 +47,25 @@ express()
                         "context" : item.image.contextLink
                     };
         });
+        res.end(JSON.stringify(results));
         
-        res.end(JSON.stringify(result));
-    },0,10);
+    },offset-10,offset);
     
+})
+.get("/api/latest/imagesearch",(req, res) => {
     
+    searchTerms
+    .find({}, {"term" : 1, "when" : 1})
+    .sort({ $natural:-1 })
+    .limit(10)
+    .toArray((err,data) => {
+        if (err) {
+            res.end();
+            throw err;
+        } else {
+            res.end(JSON.stringify(data));
+        }
+    });
 })
 .listen(port, () => {
   console.log('ISA app listening on port ',port,'!');
